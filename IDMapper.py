d008f7667017e05c21e8bd5b507209cbb7a9a11d
Dave MacFarlane <driusan@gmail.com>
2014-06-06 18:00:15 +0200

    Rewrote list of mappings to use wxListCtrl

--- IDMapper.py
+++ IDMapper.py
@@ -26,42 +26,54 @@ class simpleapp_wx(wx.Frame):
         #self.Bind(wx.EVT_TEXT_ENTER, self.OnPressEnter, self.candidatename)
 
         button = wx.Button(self,-1,label="Add candidate")
-        self.sizer.Add(button, (0,2))
+        self.sizer.Add(button, (0,2), (1, 1), wx.EXPAND)
         self.Bind(wx.EVT_BUTTON, self.AddIdentifierEvent, button)
 
-        button = wx.Button(self, -1, label="Save map")
-        self.Bind(wx.EVT_BUTTON, self.SaveMapEvent, button)
-        self.sizer.Add(button, (1, 2))
+        #button = wx.Button(self, -1, label="Save map")
+        #self.Bind(wx.EVT_BUTTON, self.SaveMapEvent, button)
+        #self.sizer.Add(button, (1, 2))
 
-        xmldoc = minidom.parse('candidates.xml')
 
-        itemlist = xmldoc.getElementsByTagName('Candidate')
+        self.datatable = wx.ListCtrl(self, -1, style=wx.LC_REPORT|wx.SUNKEN_BORDER)
+        self.datatable.InsertColumn(0, "Identifier")
+        self.datatable.InsertColumn(1, "Real Name")
+        self.sizer.Add(self.datatable, (2, 0), (1, 2), wx.EXPAND)
 
-        self.i = 1
+        self.LoadXML("candidates.xml")
 
-        for s in itemlist:
-            identifier = s.getElementsByTagName("Identifier")[0].firstChild.nodeValue
-            realname = s.getElementsByTagName("RealName")[0].firstChild.nodeValue
-
-            self.AddIdentifierAction(identifier, realname)
             
         self.sizer.AddGrowableCol(0)
         self.SetSizerAndFit(self.sizer)
-        self.SetSizeHints(-1,self.GetSize().y,-1,self.GetSize().y );
-        self.candidatename.SetFocus()
-        self.candidatename.SetSelection(-1,-1)
+        #self.SetSizeHints(-1,self.GetSize().y,-1,self.GetSize().y );
+        #self.candidatename.SetFocus()
+        #self.candidatename.SetSelection(-1,-1)
         self.Show(True)
 
-    def SaveMapEvent(self, event):
-        #import pdb; pdb.set_trace()
+    def LoadXML(self, file):
+        try:
+            xmldoc = minidom.parse(file)
+            itemlist = xmldoc.getElementsByTagName('Candidate')
+            for s in itemlist:
+                identifier = s.getElementsByTagName("Identifier")[0].firstChild.nodeValue
+                realname = s.getElementsByTagName("RealName")[0].firstChild.nodeValue
+
+                self.AddIdentifierAction(identifier, realname, False)
+        except:
+            pass
+
+    def SaveMapAction(self):
         f = open("candidates.xml", "w")
         f.write("<?xml version=\"1.0\"?>\n<data>\n")
+        print self.IDMap
         for key in self.IDMap:
             f.write("\t<Candidate>\n")
             f.write("\t\t<Identifier>%s</Identifier>\n" % key)
             f.write("\t\t<RealName>%s</RealName>\n" % self.IDMap[key])
             f.write("\t</Candidate>\n")
         f.write("</data>")
+
+    def SaveMapEvent(self, event):
+        self.SaveMapAction()
 
 
 
@@ -70,17 +82,15 @@ class simpleapp_wx(wx.Frame):
         id = self.candidateid.GetValue()
         self.AddIdentifierAction(id, name)
 
-    def AddIdentifierAction(self, candid, realname):
-        identifierstring  = u"%s : %s" % (candid, realname)
-        label = wx.StaticText( self, -1, label=identifierstring)
-        label.SetBackgroundColour(wx.BLUE)
-        label.SetForegroundColour(wx.WHITE)
-
-        self.sizer.Add( label, (self.i,0),(1,2), wx.EXPAND )
-        self.i += 1
-
-        self.sizer.Fit(self)
+    def AddIdentifierAction(self, candid, realname, save=True):
         self.IDMap[candid] = realname
+
+        idx = self.datatable.InsertStringItem(0, candid)
+        self.datatable.SetStringItem(idx, 1, realname)
+
+        if(save):
+            self.SaveMapAction()
+        self.sizer.Fit(self)
 
 if __name__ == "__main__":
     app = wx.App()
