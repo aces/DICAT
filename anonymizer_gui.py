3d98c8a30b6be51f1c5c8f8e57b6e19d7036454e
olgatsib <olgatsib@gmail.com>
2014-06-27 12:48:19 -0400

    Change to tkinter

--- anonymizer_gui.py
+++ anonymizer_gui.py
@@ -52,7 +52,7 @@ class dicom_anonymizer(Tkinter.Frame):
         height = win.winfo_height()
         x = (win.winfo_screenwidth() // 2) - (width // 2)
         y = (win.winfo_screenheight() // 2) - (height // 2)
-        win.geometry('{}x{}+{}+{}'.format(width, height, x, y))  
+        win.geometry('%dx%d+%d+%d' % (width, height, x, y))  
     
     def askdirectory(self):
 
