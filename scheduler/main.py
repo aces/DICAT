472eb7419908874ecec3593afcc645773a55c1cd
Pierre-Emmanuel Morin <pierre-emmanuel.morin@criugm.qc.ca>
2015-06-04 12:05:03 -0400

    Update main.py

--- scheduler/main.py
+++ scheduler/main.py
@@ -1,1 +1,10 @@
-test
+#!/usr/bin/env python
+
+from Tkinter import Tk
+import userinterface
+
+
+root = Tk()
+app = userinterface.ApplicationGUI(root)
+root.protocol("WM_DELETE_WINDOW", app.quitapplication)
+app.mainloop()
