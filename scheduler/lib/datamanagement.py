2a1e94323b1c2c2f0e39be1ee469506a3926b44b
PierreEMorin <pierre-emmanuel.morin@criugm.qc.ca>
2015-07-14 14:16:09 -0400

    Changed names for data files (candidate for candidatedata and studysetup.db for studydata) for consistency and avoid confusion between candidate and candidate.py class file

--- scheduler/lib/datamanagement.py
+++ scheduler/lib/datamanagement.py
@@ -21,24 +21,24 @@ def readdata(datafile):
 
 #Specific functions
 def readcandidatedata():
-    db = shelve.open("candidate")
+    db = shelve.open("candidatedata")
     return db
 
 
 def savecandidatedata(data):
-    db = shelve.open("candidate")
+    db = shelve.open("candidatedata")
     for key in data:
         db[data[key].uid] = data[key]
     db.close()
 
 
 def readstudydata():
-    db = shelve.open("studysetup.db")
+    db = shelve.open("studydata")
     return db
 
 
 def savestudydata(data):
-    db = shelve.open("studysetup.db")
+    db = shelve.open("studydata")
     for key in data:
         db[data[key].uid] = data[key]
     db.close()
