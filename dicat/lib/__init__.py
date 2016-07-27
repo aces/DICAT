9f7cf83df3b57f6e6711503fca6a5955fc2c8a7c
Cécile Madjar <cecile.madjar@gmail.com>
2016-07-27 11:30:51 -0400

    Cleaned up folowing files: 	modified:   dicat/lib/__init__.py 	deleted:    dicat/lib/classtool.py 	modified:   dicat/lib/config.py 	modified:   dicat/lib/multilanguage.py 	modified:   dicat/ui/__init__.py

--- dicat/lib/__init__.py
+++ dicat/lib/__init__.py
@@ -1,8 +1,10 @@
 """
-From the documentation at https://docs.python.org/2/tutorial/modules.html#packages
+From https://docs.python.org/2/tutorial/modules.html#packages documentation
 
-The __init__.py files are required to make Python treat the directories as containing packages; this is done to prevent
-directories with a common name, such as string, from unintentionally hiding valid modules that occur later on the module
-search path. In the simplest case, __init__.py can just be an empty file, but it can also execute initialization code
-for the package or set the __all__ variable, described later.
+The __init__.py files are required to make Python treat the directories as
+containing packages; this is done to prevent directories with a common name,
+such as string, from unintentionally hiding valid modules that occur later on
+the module search path. In the simplest case, __init__.py can just be an empty
+file, but it can also execute initialization code for the package or set the
+__all__ variable, described later.
 """
--- dicat/lib/config.py
+++ dicat/lib/config.py
@@ -1,6 +1,6 @@
 """
-This file will contain global variables that can be used in all class, assuming
-the config.py script gets imported.
+This file will contain global variables that can be used in all classes,
+assuming the config.py script gets imported.
 Access to the variable would then be config.variablename
 """
 
