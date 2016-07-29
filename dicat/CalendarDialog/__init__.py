208da4743d24b6ec1de98ef219986b95eeece3ac
Cécile Madjar <cecile.madjar@gmail.com>
2016-07-29 17:43:36 -0400

    Included a date picker for the date of birth using the code made available on https://github.com/moshekaplan/tkinter_components

---
+++ dicat/CalendarDialog/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'cmadjar'
+
+"""
+These scripts have been downloaded from the following GitHub repository and
+allows to have date picker widgets and maybe some calendar functionality?
+https://github.com/moshekaplan/tkinter_components
+"""
